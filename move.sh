ad1052652d3d52b83a8fe3e04d0d5b7729e4f62a
Muritala David <davmuri1414@gmail.com>
2024-02-21 22:01:13 +0100

    rm arm arch

--- move.sh
+++ move.sh
@@ -1,10 +1,14 @@
 mkdir -p builds
 
+ls
+
+ls builds
+
 mv brimble-linux-x64 builds/
 
-mv brimble-linux-arm64 builds/
+# mv brimble-linux-arm64 builds/
 
-mv brimble-macos-arm64 builds/
+# mv brimble-macos-arm64 builds/
 
 mv brimble-macos-x64 builds/
 
